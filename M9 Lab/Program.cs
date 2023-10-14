a5d4ea82f61cfaa60ef5ba46a2ccdf74c89dbb17
bwk5140 <brianwkarimi@gmail.com>
2023-10-14 10:51:50 +0300

    Included execution sample to main program

--- M9 Lab/Program.cs
+++ M9 Lab/Program.cs
@@ -8,3 +8,83 @@
         }
     }
 }
+/*
+ * Mocha Execution Sample:
+ * Available coffee options:
+                        1: Regular
+                        2: Mocha
+                        3: Cappuccino
+
+***Additional programs can be installed via usb***
+
+Mocha
+Add condiment? (Y/N) y
+Condiments available:
+                1: Cream
+                2: Vanilla
+                3: Chocolate
+
+Enter condiment 1:
+Cream
+Add more? (Y/N) y
+Enter condiment 2:
+Vanilla
+Add more? (Y/N) n
+
+LED Value: 11
+
+**Grinding coffee**
+Total time: 8 seconds
+
+Water temperature: 150 degrees Fahrenheit.
+
+Coffee ready!
+Led Value: 0
+
+
+Total price: $4.75
+ */
+
+
+/*
+ * Espresso Execution Sample:
+ * Available coffee options:
+                        1: Regular
+                        2: Mocha
+                        3: Cappuccino
+
+***Additional programs can be installed via usb***
+
+Espresso
+Add condiment? (Y/N) y
+Condiments available:
+                1: Cream
+                2: Vanilla
+                3: Chocolate
+
+Enter condiment 1:
+Cream
+Add more? (Y/N) y
+Enter condiment 2:
+Vanilla
+Add more? (Y/N) y
+Enter condiment 3:
+Chocolate
+Add more? (Y/N) y
+Enter condiment 4:
+Chocolate
+Add more? (Y/N) n
+
+LED Value: 13
+
+**Grinding coffee**
+Total time: 5 seconds
+
+Water temperature: 200 degrees Fahrenheit.
+
+Coffee ready!
+Led Value: 0
+
+
+Total price: $7.75
+ */
